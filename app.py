74ba2a2416bf98b190d879b7ab415b38bc4353b3
65070018 กิตติ์ชินทักษ์ <112687566+rmdir123@users.noreply.github.com>
2024-07-27 14:53:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ async def get_book(book_id: int, db: Session = Depends(get_db)):
 @router_v1.post('/books')
 async def create_book(book: dict, response: Response, db: Session = Depends(get_db)):
     # TODO: Add validation
-    newbook = models.Book(title=book['title'], author=book['author'], year=book['year'], is_published=book['is_published'])
+    newbook = models.Book(title=book['title'], author=book['author'], year=book['year'], is_published=book['is_published'], description=book['description'], summary=book['summary'], booktype=book['booktype'])
     db.add(newbook)
     db.commit()
     db.refresh(newbook)
